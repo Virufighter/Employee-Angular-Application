4c256aae948bcd721b9db41c37181836611ad0cb
Virufighter <alvinpoulose4@gmail.com>
2022-11-24 15:53:28 +0530

    entry

---
+++ src/app/empentry/empentry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empentry',
+  templateUrl: './empentry.component.html',
+  styleUrls: ['./empentry.component.css']
+})
+export class EmpentryComponent {
+
+}
