c8b0395a1ae5bc4bb98da94ab93aa565f644ac57
Virufighter <alvinpoulose4@gmail.com>
2022-11-24 16:15:00 +0530

    entry

---
+++ src/app/empedit/empedit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empedit',
+  templateUrl: './empedit.component.html',
+  styleUrls: ['./empedit.component.css']
+})
+export class EmpeditComponent {
+
+}
