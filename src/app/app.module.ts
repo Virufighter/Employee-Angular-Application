5e5823d6907e8ba79328f8db3a12738d725e9ee6
Virufighter <alvinpoulose4@gmail.com>
2022-11-24 16:07:43 +0530

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { EmpentryComponent } from './empentry/empentry.component';
+import { EmpviewComponent } from './empview/empview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLoginComponent,
-    EmpentryComponent
+    EmpentryComponent,
+    EmpviewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/empview/empview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empview',
+  templateUrl: './empview.component.html',
+  styleUrls: ['./empview.component.css']
+})
+export class EmpviewComponent {
+
+}
