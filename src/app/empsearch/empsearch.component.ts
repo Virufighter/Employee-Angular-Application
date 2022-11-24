569acd188b7648044d0d194df9f8831b657dc69c
Virufighter <alvinpoulose4@gmail.com>
2022-11-24 16:10:21 +0530

    entry

---
+++ src/app/empsearch/empsearch.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empsearch',
+  templateUrl: './empsearch.component.html',
+  styleUrls: ['./empsearch.component.css']
+})
+export class EmpsearchComponent {
+
+}
